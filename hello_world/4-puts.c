ed9f0e2f1639a38b4f4d014316f9c06322a91812
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-10-21 10:26:57 +0000

    test4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-ggc puts Programming is like building a multilingual puzzle
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Calls puts and recieves value returned by puts after its
+ * execution.
+ * Return: Always return 0 to indicate success
+ */
+int main(void)
+{
+	puts("\"ALX is the best");
+	return (0);
+}
