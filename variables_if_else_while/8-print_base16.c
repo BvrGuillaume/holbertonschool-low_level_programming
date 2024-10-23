ea1b7e380aa09592fdf37d2d6ea1c5be8adf6fb2
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-10-23 13:35:38 +0000

    test8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 48;
+	char l = 'a';
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	while (l <= 'f')
+	{
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
