1d2b105ea4b55a07d088379f43ec092b840d45ab
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-10-23 12:00:20 +0000

    test5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int N = 48;
+
+	while (N <= 57)
+	{
+		putchar(N);
+		N++;
+	}
+	putchar('\n');
+	return (0);
+}
