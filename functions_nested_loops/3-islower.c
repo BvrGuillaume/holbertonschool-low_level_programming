3713cbfbdaea39157ebcb4032a7964495b674a9e
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-10-25 12:08:23 +0000

    test3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <ctype.h>
 /**
-* int _islower(in c) - c lowercase
+*_islower - c lowercase
+* @c: letter tested
 * Return: Always 0 (Success)
-* Description: if c is lowercase return 1 else return 0
+* Description: if c is lowercase return 1 else return 
 */
 int _islower(int c)
 {
