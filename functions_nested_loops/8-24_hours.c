f1ffc90017dc1c6c72eb4a4472699e415ba7f058
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-10-27 09:55:08 +0000

    test8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * jack_bauer - print every minutes of the day
+ * Return: Always 0(Success)
+ */
+void jack_bauer(void)
+{
+	int a, b;
+	a = 0;
+
+	while (a < 24)
+	{
+		b = 0;
+		while (b < 60)
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+			b++;
+		}
+		a++;
+	}
+}
