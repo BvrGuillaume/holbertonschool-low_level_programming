6cbeb0b5d103cfff2f80b6a72662d3184878a4d0
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-10-27 09:10:40 +0000

    test7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,12 @@
  */
 int print_last_digit(int n)
 {
-	while (n >= 10)
-		n /= 10;
-	return (n % 10);
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
