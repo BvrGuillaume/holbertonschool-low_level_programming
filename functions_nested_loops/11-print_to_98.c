32cea6bc192290588af26fde8e236298528f2db4
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-10-27 16:35:09 +0000

    test11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* main - check the code 
+* print_to_98 - Prints all natural numbers from n to 98
+* Return: Always 0(Success)
+*/
+int main(void)
+{
+		print_to_98 (0);
+		print_to_Ã98 (98);
+		print_to_98 (111);
+		print_to_98 (81);
+		print_to_98 (-10);
+return (0);
+}
