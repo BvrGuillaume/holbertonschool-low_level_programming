13a3f3146d24d8bcddb111a875c4e43e46219747
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-10-28 11:27:37 +0000

    printnumber

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_numbers - check the code
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		{
+			_putchar((n) + '0');
+		}
+	{
+	_putchar('\n');
+	}
+}
