2f4443dff57c49bdc175e772aa363ec4915cce6d
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-10-28 09:29:01 +0000

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - check the code
+ @c: letter tested
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
