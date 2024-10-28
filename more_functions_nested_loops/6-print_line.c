073b55b8958f054e57e06a176fd394bbdd707ef3
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-10-28 19:36:24 +0000

    printline

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -6,9 +6,19 @@
  */
 void print_line(int n)
 {
-	int n
+	int a;
 
-	if ( n <= '0')
+	if (n > 0)
+	{
+	for (a = 0; a < n; a++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
 
-
-		_putchar('\n')
+}
