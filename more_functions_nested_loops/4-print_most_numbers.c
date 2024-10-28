568805ba5673ebd4a58c0222cb5618807bf43370
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-10-28 13:15:40 +0000

    mostnumber

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_most_numbers - check the code
+ */
+void print_most_numbers(void)
+{
+	char number = '0';
+
+	while (number <= '9')
+	{
+		putchar(number);
+		number++;
+		if (number == '2' || number == '4')
+			number++;
+	}
+	putchar('\n');
+}
