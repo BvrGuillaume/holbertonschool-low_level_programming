e4b14c44fe0ca27ac3e3890d407fb0992f17dd6e
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-10-28 10:05:23 +0000

    digit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -3,11 +3,11 @@
 /**
  * _isdigit - check the code
  * @c: letter tested
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int _isdigit(int c)
 {
-	if (isdigit(c))
+	if (_isdigit(c))
 		return (1);
 	else
 		return (0);
