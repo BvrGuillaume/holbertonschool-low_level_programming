2e829fffe330c59c87faa3ce49990c57a6dac998
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-10-29 13:21:10 +0000

    fizz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - print fizz buzz
+ * Return: Always 0(Success) 
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a %15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (a % 3 == 0)
+			printf("Fizz ");
+		else 
+		{
+			printf("%d ", a);
+		}
+	}
+	printf("\n");
+	return (0);
+}
