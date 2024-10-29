72780e761d28620232b9c6ca45f811e637c71769
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-10-29 14:23:12 +0000

    triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_triangle
+ * @size: letter checked
+ */
+
+void print_triangle(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				if (b < size - a - 1)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+				_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
