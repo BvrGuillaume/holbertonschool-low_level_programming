f2ec3e829c8dffe064ca94d5da165292d52880de
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-11-04 18:33:19 +0000

    copymem

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memcpy - copy function
+ * @dest: destination
+ * @src: source
+ * @n: byte
+ * Return: char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *i = dest;
+
+	while (n--)
+	{
+		*i++ = *src;
+	}
+
+	return (dest);
+}
