a29008292bf984d80b566defc95b7298db3f8ee0
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-11-11 11:46:13 +0100

    array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h">
+#include <stdlib.h>
+/**
+ * create_array - function
+ * @size: variable
+ * @c: variable
+ * Return: char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array
+	array = malloc((size +1) * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	array[i] = '\0';
+			return (array);
+}
