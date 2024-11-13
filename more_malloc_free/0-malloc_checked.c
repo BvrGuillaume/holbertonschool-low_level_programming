87a20e8c1a921e77b4a06f2667502d53596b4007
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-11-13 11:49:51 +0100

    allocatememory

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - code check
+ * @b: int checked
+ * Return: void
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
